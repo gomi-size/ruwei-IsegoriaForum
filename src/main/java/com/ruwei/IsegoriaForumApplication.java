93db562200dcec06be620da61c1d97010fcd5da1
wudi <3136124236@qq.com>
2026-07-13 20:34:54 +0800

    项目初始化

---
+++ src/main/java/com/ruwei/IsegoriaForumApplication.java
@@ -0,0 +1,13 @@
+package com.ruwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsegoriaForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsegoriaForumApplication.class, args);
+    }
+
+}
